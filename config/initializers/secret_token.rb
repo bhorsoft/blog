a89f98ebf3014d1fb2ea5c1a0ae8ea5d73be7918
bhorsoft <bhorsoft@yahoo.com>
2014-08-14 15:42:50 -0400

    Initial commit of repo project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Repo::Application.config.secret_key_base = '880af85c685946fdf1fe705dd39a103b61284dffcd15ecfc0278d4fd5d1227ea6e395caf24e24eec443dba4cc4753de8ee3f1aca71a1f6cb788f9959bcddfeee'
